e0c96e6c2f3a0195d8f8920452985da794758dbd
Daniel Villanueva Jimenez <dvillaj@gmail.com>
2023-03-17 09:18:05 +0100

    enumerate

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ api_key=config.get("shlink", "api_key")
 
 shlink = Shlink(url_host, api_key)
 
-shlink.backup("shlink.backup")
+# shlink.backup("shlink.backup")
 links = shlink.restore("shlink.backup")
 
-links = shlink.list_links()
-for link in links:
+# links = shlink.list_links()
+for n, link in enumerate(links):
     shortCode = link['shortCode']
-    print(f"Processing {shortCode}")
-    #shlink.delete_link(shortCode)
+    print(f"Processing {n + 1} - {shortCode}")
+    
+    # shlink.delete_link(shortCode)
     #shlink.add_link(link['title'], link['longUrl'], shortCode, link['tags'])
